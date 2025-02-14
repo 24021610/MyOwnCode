d13ebd503d1d8a96308e4b5a5e15fee084d88acc
24021610 <24021610@vnu.edu.vn>
2025-02-14 08:54:59 +0700

    Create header.h

---
+++ XY_Game/header.h
@@ -0,0 +1,19 @@
+#ifndef DEFS
+#define DEFS
+
+#include <SDL.h>
+#include "graphics.h"
+
+
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 700;
+const char* WINDOW_TITLE = "Hello World!";
+#define BOARD_X 10
+#define BOARD_Y 10
+#define CELL_SIZE 30
+#define BOARD_SIZE 18
+#define EMPTY_CELL ' '
+#define O_CELL 'o'
+#define X_CELL 'x'
+
+#endif // DEFS
